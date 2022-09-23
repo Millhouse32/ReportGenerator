7c3e958d6211ac99630f9b4d3e1dee2b8ac37566
Nicholas Miller <nicholas_miller@goodyear.com>
2022-09-23 08:14:16 -0400

    Implement socket server

--- ReportGenerator/Program.cs
+++ ReportGenerator/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace ReportGenerator
+{
+    class Program 
+    { 
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
