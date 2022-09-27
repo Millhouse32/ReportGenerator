b816d4c6067245441cd7378cce6e7a491363ef55
Nicholas Miller <nlm82@uakron.edu>
2022-09-27 13:08:44 -0400

    Add generate CSV method

--- ReportGenerator/Program.cs
+++ ReportGenerator/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Data;
 using System.Net.Http;
 using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Text.Json;
 using System.Xml;
 using System.Xml.Linq;
+using Aspose.Cells;
+using Aspose.Cells.Utility;
+using Newtonsoft.Json;
+using static System.Net.Mime.MediaTypeNames;
+using SaveOptions = Aspose.Cells.SaveOptions;
 
 namespace ReportGenerator
 {
@@ -16,12 +22,45 @@ namespace ReportGenerator
             {""name"": ""john dow"", ""age"" : ""25""} ]";
 
 
+            ToXML(data);
 
+            ToEXCEL(data);
+            
+        }
+
+        public static void ToXML(string json)
+        {
             XmlDocument doc = new XmlDocument();
 
-            var xml = XDocument.Load(JsonReaderWriterFactory.CreateJsonReader(Encoding.ASCII.GetBytes(data), new XmlDictionaryReaderQuotas()));
+            var xml = XDocument.Load(JsonReaderWriterFactory.CreateJsonReader(Encoding.ASCII.GetBytes(json), new XmlDictionaryReaderQuotas()));
 
             Console.WriteLine(xml);
         }
+
+        public static void ToEXCEL(string json)
+        {
+            DataTable dt = JsonConvert.DeserializeObject<DataTable>(json);
+
+            StringBuilder fileContent = new StringBuilder();
+
+            foreach (var col in dt.Columns)
+            {
+                fileContent.Append(col.ToString() + ",");
+            }
+
+            fileContent.Replace(",", System.Environment.NewLine, fileContent.Length - 1, 1);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                foreach (var col in row.ItemArray)
+                {
+                    fileContent.Append("\"" + col.ToString() + "\",");
+
+                }
+                fileContent.Replace(",", System.Environment.NewLine, fileContent.Length - 1, 1);
+                System.IO.File.WriteAllText("people.csv", fileContent.ToString());
+            }
+        }
+
     }
 }
