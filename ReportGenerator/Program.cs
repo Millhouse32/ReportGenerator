18cb259f8b6f788d9d4daea26cd8dba15cbd570e
Nicholas Miller <nlm82@uakron.edu>
2022-09-27 15:56:11 -0400

    Update readme

--- ReportGenerator/Program.cs
+++ ReportGenerator/Program.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Data;
-using System.Net.Http;
-using System.Runtime.Serialization.Json;
+﻿using System.Data;
 using System.Text;
-using System.Text.Json;
-using System.Xml;
-using System.Xml.Linq;
-using Aspose.Cells;
-using Aspose.Cells.Utility;
 using Newtonsoft.Json;
 using ReportGenerator.Workers;
 
@@ -17,48 +9,12 @@ namespace ReportGenerator
     { 
         public static void Main(string[] args)
         {
-            string data = "{\n    \"email\":[\n        {\n            \"type\":\"home\",\n            \"name\":\"john.doe@gmail.com\"\n        },\n        {\n            \"type\":\"work\",\n            \"name\":\"jdoe@gmail.com\"\n        }\n    ]\n}";
+            string data = "{\n\"email\":[\n{\n\"type\":\"home\",\n\"name\":\"john.doe@gmail.com\"\n},\n{\n\"type\":\"work\",\n\"name\":\"jdoe@gmail.com\"\n}\n]\n}";
 
 
             ToXML.ConvertToXML(data, "email");
 
-            //ToEXCEL(data);
-            
+            ToCSV.ConvertToCSV(data, "email");
         }
-
-        //public static void ToXML(string json)
-        //{
-        //    XmlDocument doc = new XmlDocument();
-
-        //    var xml = XDocument.Load(JsonReaderWriterFactory.CreateJsonReader(Encoding.ASCII.GetBytes(json), new XmlDictionaryReaderQuotas()));
-
-        //    Console.WriteLine(xml);
-        //}
-
-        public static void ToEXCEL(string json)
-        {
-            DataTable dt = JsonConvert.DeserializeObject<DataTable>(json);
-
-            StringBuilder fileContent = new StringBuilder();
-
-            foreach (var col in dt.Columns)
-            {
-                fileContent.Append(col.ToString() + ",");
-            }
-
-            fileContent.Replace(",", System.Environment.NewLine, fileContent.Length - 1, 1);
-
-            foreach (DataRow row in dt.Rows)
-            {
-                foreach (var col in row.ItemArray)
-                {
-                    fileContent.Append("\"" + col.ToString() + "\",");
-
-                }
-                fileContent.Replace(",", System.Environment.NewLine, fileContent.Length - 1, 1);
-                System.IO.File.WriteAllText("people.csv", fileContent.ToString());
-            }
-        }
-
     }
 }
--- ReportGenerator/Workers/ToCSV.cs
+++ ReportGenerator/Workers/ToCSV.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Aspose.Cells;
+using Aspose.Cells.Utility;
+
 namespace ReportGenerator.Workers
 {
-    public static class ToCSV
+    public static class ToXLSX
     {
-        public static void ConvertToCSV(string json, string filename)
+        public static void ConvertToXLSX(string json, string filename)
         {
-            filename += ".csv";
+            filename += ".Xlsx";
+
+            var workbook = new Workbook();
+
+            var worksheet = workbook.Worksheets[0];
+
+            var layoutOptions = new JsonLayoutOptions();
+
+            layoutOptions.ArrayAsTable = true;
+
+            JsonUtility.ImportData(json, worksheet.Cells, 0, 0, layoutOptions);
+
+            workbook.Save(filename, SaveFormat.Xlsx);  
         }
     }
 }
-
