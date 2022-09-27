0d1e616d6dac4ef7b03019ac0656abc743c1f5d9
Nicholas Miller <nlm82@uakron.edu>
2022-09-27 12:11:28 -0400

    Switch nuget package to WatsonWebsocket and set up client class

---
+++ ReportGenerator/Client.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using WatsonWebsocket;
+
+namespace ReportGenerator
+{
+    public class Client
+    {
+        public Client(string serverIP, int port)
+        {
+            WatsonWsClient client = new WatsonWsClient(serverIP, port, true);
+            client.ServerConnected += ServerConnected;
+            client.ServerDisconnected += ServerDisconnected;
+            client.MessageReceived += MessageReceived;
+            client.Start();
+        }
+
+        private static void ServerConnected(object sender, EventArgs args)
+        {
+            Console.WriteLine("Server connected!");
+        }
+
+        private static void ServerDisconnected(object sender, EventArgs args)
+        {
+            Console.WriteLine("Server disconnected!");
+        }
+
+        private static void MessageReceived(object sender, MessageReceivedEventArgs args)
+        {
+            Console.WriteLine("Message from server:" + Encoding.UTF8.GetString(args.Data));
+        }
+    }
+}
